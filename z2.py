62458627b8cc91b24fdd1a392d54b10a6603be9d
mathbike <mathbike8@gmail.com>
2022-12-02 18:37:45 -0500

    d

--- z2.py
+++ z2.py
@@ -1,6 +1,12 @@
-import re
+import sys, re
 
-txt = "The rain in Spain"
-x = re.sub("\s", "9", txt)
-print(x)
+with open("0__init__.py", "r") as file_input:
+    with open("__init__.py", "w") as file_output: 
+        for line in file_input:
+            x = re.sub("$NAME", sys.argv[1], line)
+            file_output.write(line)
+
+""" txt = "The rain in Spain"
+x = re.sub(sys.argv[1], txt)
+print(x) """
 

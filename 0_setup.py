2c2dbcff774ec2476b81ffd5efb30b07b261ddd8
mathbike <mathbike8@gmail.com>
2022-12-02 19:31:55 -0500

    d

--- 0_setup.py
+++
@@ -1,8 +0,0 @@
-import sys, re
-
-with open("0__init__.py", "r") as file_input:
-    with open("__init__.py", "w") as file_output: 
-        for line in file_input:
-            if line.lstrip().startswith('#'):
-                continue
-            file_output.write(line)
--- z.py
+++ z.py
@@ -1,10 +1,12 @@
+import sys
+
 # Read in the file
 with open('__init__.py', 'r') as file :
-  filedata = file.read()
+    filedata = file.read()
 
 # Replace the target string
-filedata = filedata.replace('$NAME', 'abcd')
+filedata = filedata.replace('$NAME', sys.argv[1])
 
 # Write the file out again
 with open('__init__.py', 'w') as file:
-  file.write(filedata)
+    file.write(filedata)
